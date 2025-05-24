ecc90eda0ddd75d4c0b34ca889f9d846f6f7e42f
Amin <min123kabir@gmail.com>
2025-05-24 08:59:56 +0100

    done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
   return (
-    <div className="px-14 sm:px-24 md:px-36 py-24 text-md bg-gradient-to-bl from-cyan-200 to-blue-200" id="About">
+    <div className="px-14 sm:px-24 md:px-36 py-24 text-lg bg-gradient-to-bl from-cyan-200 to-blue-200" id="About">
       <h2 className="text-center text-2xl sm:text-5xl mb-10 font-caveat"><span className="underline">Abo</span>ut me</h2>
       <p>My name is Amin Abdulkabir, and I am a devoted full-stack developer with a passion for crafting scalable, 
         user-centric solutions. With a year of experience in the industry, I've honed my skills in various 
@@ -10,8 +10,16 @@ function About() {
       </p>
       <br />
       <p>Throughout my career, I have dedicated a reasonable quantity of time towards self development, and thus, have reached a
-        point whereby I believe my expertise would be one of a kind in this dynamic yet broad industry.
+        point whereby I believe my expertise would be one of a kind in this dynamic yet broad industry. 
       </p>
+      < br />
+      <p>
+        I have 2 years of hands-on experience in building projects, having provided quality frontend and backend for various projects 
+        and worked with several clients. find attacehd, the link to my resume for more in-depth reference
+      </p>
+      <br />
+      <a href="https://docs.google.com/document/d/1_7DEIDy1SmDHkb51i6HGL-L6azjDftasfxDgGTtPrfk/edit?usp=sharing" target='_blank'><button  className="bg-blue-400 text-black font-semibold h-12 w-28 rounded shadow-2xl">Resume</button></a>
+
     </div>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,8 +23,8 @@ function Projects() {
             {project.tools.map(listTools)}
           </div>
           <div className="flex justify-between mt-8 text-base sm:text-xl">
-            <a href={project.live} target="_blank"><button className=" h-10 w-24 sm:w-32 bg-blue-200 hover:bg-blue-300 rounded">Live <ArrowOutwardIcon /></button></a>
-            <a href={project.code} target="_blank"><button className="border-2 border-blue-200 h-10 w-24 sm:w-32 hover:bg-blue-200 duration-300" >Code {project.github}</button></a>
+            <a href={project.live} target="_blank"><button className=" h-10 w-24 sm:w-32 bg-blue-200 hover:bg-blue-300 rounded duration-300">Live <ArrowOutwardIcon /></button></a>
+            {project.code && <a href={project.code} target="_blank"><button className="border-2 border-blue-200 h-10 w-24 sm:w-32 hover:bg-blue-200 duration-300" >Code {project.github}</button></a>}
           </div>
         </div>
       </div>
